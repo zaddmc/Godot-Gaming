17ade3b4c136e54514dd2c4369820882330a6845
Zadd ZC <davidbk927@gmail.com>
2024-12-01 01:42:19 +0100

    if you cant do it 'the propper' way just do it the easy way

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 3.0
+const SPEED = 2.0
 const JUMP_VELOCITY = 4.5
 var target 
 
--- scripts/horde_main.gd
+++ scripts/horde_main.gd
@@ -10,7 +10,8 @@ func _ready() -> void:
 	call_deferred("spawn_enemies")
 
 func _process(delta: float) -> void:
-	print(get_spawn_location())
+	spawn_enemy(Enemy_Type.Rat)
+
 
 func spawn_enemies():
 	for i in range(20):
@@ -27,25 +28,17 @@ func spawn_enemy(enemy_type:Enemy_Type):
 
 func get_spawn_location() -> Vector3:
 	#var player_position = PLAYER.global_position
-	
 	var total_top_left = $plane/Top_left.global_position
 	var total_bottom_right = $plane/Bottom_right.global_position
 	var anti_top_left = $Player/Spawning_Cylinders/Anti_top_left.global_position
 	var anti_bottom_right = $Player/Spawning_Cylinders/Anti_bottom_right.global_position
+	#var anti_spawn_area = $Player/Spawning_Cylinders/Anti_Spawn_area.get_aabb().size
 
-	var x_range = [total_top_left.x, anti_top_left.x, total_bottom_right.x, anti_bottom_right.x]
-	var y_range = [total_top_left.z, anti_top_left.z, total_bottom_right.z, anti_bottom_right.z]
-	
-	var x_vals = []
-	if x_range[0] - x_range[1] < 0:
-		x_vals.append(randf_range(x_range[0], x_range[1]))
-	if x_range[2] - x_range[3] > 0:
-		x_vals.append(randf_range(x_range[2], x_range[3]))
-	var y_vals = []
-	if y_range[0] - y_range[1] < 0:
-		y_vals.append(randf_range(y_range[0], y_range[1]))
-	if y_range[2] - y_range[3] > 0:
-		y_vals.append(randf_range(y_range[2], y_range[3]))
-	
-	
-	return Vector3(x_vals.pick_random(), 0, y_range.pick_random())
+	var point = Vector3(randf_range(total_top_left.x, total_bottom_right.x), 0, randf_range(total_top_left.z, total_bottom_right.z))
+	while is_in_square(point, anti_top_left, anti_bottom_right):
+		point = Vector3(randf_range(total_top_left.x, total_bottom_right.x), 0, randf_range(total_top_left.z, total_bottom_right.z))
+
+	return point
+
+func is_in_square(point, corn1, corn2) -> bool:
+	return (corn1.x < point.x and corn2.x > point.x and corn1.z < point.z and corn2.z > point.z)
